c4a98597524cb40ba2df02d1a14440c63ccfef81
jhalva <96316268+jhalva@users.noreply.github.com>
2022-06-14 17:29:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<!-- saved from url=(0093)https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf -->
+<html style="font-size: 16px;" class="u-responsive-xl"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 9</title>
+    <link rel="stylesheet" href="./nicepage.css" media="screen">
+    <script class="u-script" type="text/javascript" src="./jquery-1.9.1.min.js.download" defer=""></script>
+    <script class="u-script" type="text/javascript" src="./nicepage.js.download" defer=""></script>
+    <meta name="generator" content="Nicepage 3.15.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="./css">
+    <style class="u-style">.u-section-2 {
+  background-image: none;
+}
+.u-section-2 .u-sheet-1 {
+  min-height: 500px;
+}
+.u-section-2 .u-layout-wrap-1 {
+  margin-top: 60px;
+  margin-bottom: 60px;
+  position: relative;
+}
+.u-section-2 .u-layout-cell-1 {
+  min-height: 472px;
+}
+.u-section-2 .u-container-layout-1 {
+  padding: 30px;
+}
+.u-section-2 .u-text-1 {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.25rem;
+  margin: 0;
+}
+.u-section-2 .u-text-2 {
+  margin: 20px 20px 0 0;
+}
+.u-section-2 .u-text-3 {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.25rem;
+  margin: 20px 0 0;
+}
+.u-section-2 .u-text-4 {
+  margin: 18px 119px 0 0;
+}
+.u-section-2 .u-text-5 {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.25rem;
+  margin: 20px 0 0;
+}
+.u-section-2 .u-text-6 {
+  margin: 20px 116px 0 0;
+}
+.u-section-2 .u-layout-cell-2 {
+  min-height: 472px;
+}
+.u-section-2 .u-container-layout-2 {
+  padding: 30px;
+}
+.u-section-2 .u-form-1 {
+  height: 317px;
+  margin: 0 -60px 0 0;
+}
+.u-section-2 .u-form-group-2 {
+  margin-left: 0;
+}
+.u-section-2 .u-form-group-3 {
+  margin-left: 0;
+}
+.u-section-2 .u-form-group-6 {
+  margin-left: 0;
+}
+.u-section-2 .u-btn-1 {
+  border-style: none;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  background-image: none;
+}
+.u-section-2 .u-social-icons-1 {
+  white-space: nowrap;
+  height: 32px;
+  min-height: 16px;
+  width: 200px;
+  min-width: 120px;
+  margin: 75px auto 0 0;
+}
+.u-section-2 .u-icon-1 {
+  height: 100%;
+}
+.u-section-2 .u-icon-2 {
+  height: 100%;
+}
+.u-section-2 .u-icon-3 {
+  height: 100%;
+}
+.u-section-2 .u-icon-4 {
+  height: 100%;
+}
+.u-section-2 .u-icon-5 {
+  height: 100%;
+}
+@media (max-width: 1199px) {
+  .u-section-2 .u-sheet-1 {
+    min-height: 430px;
+  }
+  .u-section-2 .u-layout-cell-1 {
+    min-height: 389px;
+  }
+  .u-section-2 .u-text-2 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-text-4 {
+    margin-right: 52px;
+  }
+  .u-section-2 .u-text-6 {
+    margin-right: 49px;
+  }
+  .u-section-2 .u-layout-cell-2 {
+    min-height: 389px;
+  }
+  .u-section-2 .u-form-1 {
+    margin-right: initial;
+    margin-left: initial;
+  }
+}
+@media (max-width: 991px) {
+  .u-section-2 .u-sheet-1 {
+    min-height: 353px;
+  }
+  .u-section-2 .u-layout-cell-1 {
+    min-height: 100px;
+  }
+  .u-section-2 .u-container-layout-1 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .u-section-2 .u-text-4 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-text-6 {
+    margin-right: 0;
+  }
+  .u-section-2 .u-layout-cell-2 {
+    min-height: 100px;
+  }
+  .u-section-2 .u-container-layout-2 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+@media (max-width: 767px) {
+  .u-section-2 .u-sheet-1 {
+    min-height: 1146px;
+  }
+}
+@media (max-width: 575px) {
+  .u-section-2 .u-sheet-1 {
+    min-height: 816px;
+  }
+}</style>
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "https://website397985.nicepage.io/Page-9.html"
+}</script>
+    <meta property="og:title" content="Page 9">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#617587">
+    <link rel="canonical" href="https://website397985.nicepage.io/Page-9.html">
+    <meta property="og:url" content="https://website397985.nicepage.io/Page-9.html">
+  
+</head>
+  <body class="u-body">
+    
+    <section class="u-clearfix u-palette-1-base u-section-2" id="carousel_5b1b">
+      <div class="u-clearfix u-sheet u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-size-20 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h6 class="u-text u-text-1">ligue para nós</h6>
+                  <p class="u-text u-text-2">1 (234) 567-891 <br> 1 (234) 987-654
+                  </p>
+                  <h6 class="u-text u-text-3">localização</h6>
+                  <p class="u-text u-text-4">121 Rock Sreet, 21 Avenue, <br> New York, NY 92103-9000
+                  </p>
+                  <h6 class="u-text u-text-5">Nossos melhores serviços</h6>
+                  <ul class="u-custom-list u-text u-text-6">
+                    <li>
+                      <div class="class u-list-icon">
+                        <div xmlns="http://www.w3.org/2000/svg" xmlns:="" xlink="http://www.w3.org/1999/xlink" version="1.1" xml:="" space="preserve" class="u-svg-content"> ✔ </div>
+                      </div> Transferências locais
+                    </li>
+                    <li>
+                      <div class="class u-list-icon">
+                        <div xmlns="http://www.w3.org/2000/svg" xmlns:="" xlink="http://www.w3.org/1999/xlink" version="1.1" xml:="" space="preserve" class="u-svg-content"> ✔ </div>
+                      </div> Traslados de aeroporto
+                    </li>
+                    <li>
+                      <div class="class u-list-icon">
+                        <div xmlns="http://www.w3.org/2000/svg" xmlns:="" xlink="http://www.w3.org/1999/xlink" version="1.1" xml:="" space="preserve" class="u-svg-content"> ✔ </div>
+                      </div> Excursões e passeios
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-40 u-layout-cell-2">
+                <div class="u-container-layout u-valign-top u-container-layout-2">
+                  <div class="u-expanded-width u-form u-form-1">
+                    <form action="https://nicepage.com/editor/Forms/Process" method="POST" class="u-clearfix u-form-spacing-30 u-form-vertical u-inner-form" style="padding: 0;" source="email" name="form">
+                      <input type="hidden" id="siteId" name="siteId" value="397985">
+                      <input type="hidden" id="pageId" name="pageId" value="400426">
+                      <div class="u-form-group u-form-name u-form-partition-factor-2">
+                        <label for="name-daf4" class="u-form-control-hidden u-label">Name</label>
+                        <input type="text" placeholder="Enter your Name" id="name-daf4" name="name" class="u-border-2 u-border-white u-input u-input-rectangle" required="">
+                      </div>
+                      <div class="u-form-address u-form-group u-form-partition-factor-2 u-form-group-2">
+                        <label for="address-7d20" class="u-form-control-hidden u-label">Address</label>
+                        <input type="text" placeholder="Enter your address" id="address-7d20" name="address" class="u-border-2 u-border-white u-input u-input-rectangle" required="">
+                      </div>
+                      <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-3">
+                        <label for="phone-966d" class="u-form-control-hidden u-label">Phone</label>
+                        <input type="tel" pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})" placeholder="Enter your phone (e.g. +14155552675)" id="phone-966d" name="phone" class="u-border-2 u-border-white u-input u-input-rectangle" required="">
+                      </div>
+                      <div class="u-form-email u-form-group u-form-partition-factor-2">
+                        <label for="email-daf4" class="u-form-control-hidden u-label">Email</label>
+                        <input type="email" placeholder="Enter a valid email address" id="email-daf4" name="email" class="u-border-2 u-border-white u-input u-input-rectangle" required="">
+                      </div>
+                      <div class="u-form-group u-form-message">
+                        <label for="message-daf4" class="u-form-control-hidden u-label">Message</label>
+                        <textarea placeholder="" rows="4" cols="50" id="message-daf4" name="message" class="u-border-2 u-border-white u-input u-input-rectangle" required=""></textarea>
+                      </div>
+                      <div class="u-form-agree u-form-group u-form-group-6">
+                        <input type="checkbox" id="agree-2c2f" name="agree" class="u-agree-checkbox" required="">
+                        <label for="agree-2c2f" class="u-label">I accept the <a href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf#">Terms of Service</a>
+                        </label>
+                      </div>
+                      <div class="u-align-left u-form-group u-form-submit">
+                        <a href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf#" class="u-active-grey-70 u-btn u-btn-submit u-button-style u-hover-palette-1-dark-3 u-white u-btn-1">Enviar</a>
+                        <input type="submit" value="submit" class="u-form-control-hidden">
+                      </div>
+                      <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+                      <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+                      <input type="hidden" value="" name="recaptchaResponse">
+                    </form>
+                  </div>
+                  <div class="u-social-icons u-spacing-10 u-social-icons-1">
+                    <a class="u-social-url" target="_blank" href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf"><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112"><use href="#svg-7b69"></use></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-7b69" class="u-svg-content"><path d="M56.1,0C25.1,0,0,25.1,0,56.1c0,31,25.1,56.1,56.1,56.1c31,0,56.1-25.1,56.1-56.1C112.2,25.1,87.1,0,56.1,0z M71.6,34.3h-8.2c-1.3,0-3.2,0.7-3.2,3.5v7.6h11.3l-1.3,12.9h-10V95H45V58.3h-7.2V45.4H45v-8.3c0-6,2.8-15.3,15.3-15.3l11.2,0V34.3z "></path></svg></span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf"><span class="u-icon u-icon-circle u-social-icon u-social-twitter u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112"><use href="#svg-4577"></use></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-4577" class="u-svg-content"><path d="M56.1,0C25.1,0,0,25.1,0,56.1s25.1,56.1,56.1,56.1s56.1-25.1,56.1-56.1S87.1,0,56.1,0z M83.8,47.3 c0,0.6,0,1.2,0,1.7c0,17.7-13.5,38.2-38.2,38.2c-7.6,0-14.6-2.2-20.6-6c1,0.1,2.1,0.2,3.2,0.2c6.3,0,12.1-2.1,16.7-5.7 c-5.9-0.1-10.8-4-12.5-9.3c0.8,0.2,1.7,0.2,2.5,0.2c1.2,0,2.4-0.2,3.5-0.5c-6.1-1.2-10.8-6.7-10.8-13.1c0-0.1,0-0.1,0-0.2 c1.8,1,3.9,1.6,6.1,1.7c-3.6-2.4-6-6.5-6-11.2c0-2.5,0.7-4.8,1.8-6.7c6.6,8.1,16.5,13.5,27.6,14c-0.2-1-0.3-2-0.3-3.1 c0-7.4,6-13.4,13.4-13.4c3.9,0,7.3,1.6,9.8,4.2c3.1-0.6,5.9-1.7,8.5-3.3c-1,3.1-3.1,5.8-5.9,7.4c2.7-0.3,5.3-1,7.7-2.1 C88.7,43,86.4,45.4,83.8,47.3z"></path></svg></span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf"><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112"><use href="#svg-e675"></use></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-e675" class="u-svg-content"><path d="M56.1,0C25.1,0,0,25.1,0,56.1c0,31,25.1,56.1,56.1,56.1c31,0,56.1-25.1,56.1-56.1C112.2,25.1,87.1,0,56.1,0z M90.6,73.4c0,9.6-7.8,17.5-17.5,17.5H38.6c-9.6,0-17.5-7.9-17.5-17.6V38.8c0-9.6,7.8-17.5,17.5-17.5h34.5c9.6,0,17.5,7.8,17.5,17.5 V73.4z"></path><path d="M73.1,28.9H38.6c-5.4,0-9.9,4.4-9.9,9.9v34.5c0,5.4,4.4,9.9,9.9,9.9h34.5c5.4,0,9.9-4.4,9.9-9.9V38.8 C83,33.4,78.6,28.9,73.1,28.9z M55.9,74C46,74,38,66,38,56.1c0-9.9,8-17.9,17.9-17.9c9.9,0,17.9,8,17.9,17.9 C73.8,66,65.8,74,55.9,74z M74.3,41.9c-2.3,0-4.2-1.9-4.2-4.2s1.9-4.2,4.2-4.2c2.3,0,4.2,1.9,4.2,4.2S76.6,41.9,74.3,41.9z"></path><path d="M55.9,45.8c-5.7,0-10.4,4.6-10.3,10.3c0,5.7,4.6,10.3,10.3,10.3s10.3-4.6,10.3-10.3 C66.2,50.4,61.6,45.8,55.9,45.8z"></path></svg></span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf#"><span class="u-icon u-icon-circle u-social-icon u-social-vk u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112"><use href="#svg-b519"></use></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-b519" class="u-svg-content"><path d="M56.1,0C25.1,0,0,25.1,0,56.1c0,31,25.1,56.1,56.1,56.1c31,0,56.1-25.1,56.1-56.1C112.2,25.1,87.1,0,56.1,0z M88.2,80.7l-9.8,0.1c0,0-2.1,0.4-4.9-1.5c-3.7-2.5-7.2-9.1-9.9-8.2C60.9,72,61,77.9,61,77.9s0,1.3-0.6,1.9c-0.7,0.7-2,0.9-2,0.9H54 c0,0-9.7,0.6-18.3-8.3c-9.3-9.7-17.6-29-17.6-29s-0.5-1.3,0-1.9c0.6-0.7,2.2-0.7,2.2-0.7l10.5-0.1c0,0,1,0.2,1.7,0.7 c0.6,0.4,0.9,1.2,0.9,1.2s1.7,4.3,4,8.2c4.4,7.6,6.4,9.3,7.9,8.4c2.2-1.2,1.5-10.7,1.5-10.7s0-3.5-1.1-5c-0.9-1.2-2.5-1.5-3.3-1.6 c-0.6-0.1,0.4-1.5,1.6-2.1c1.9-0.9,5.2-1,9.2-0.9c3.1,0,4,0.2,5.2,0.5c3.6,0.9,2.4,4.3,2.4,12.4c0,2.6-0.5,6.3,1.4,7.5 c0.8,0.5,2.8,0.1,7.7-8.3c2.3-4,4.1-8.6,4.1-8.6s0.4-0.8,1-1.2c0.6-0.4,1.4-0.3,1.4-0.3l11.1-0.1c0,0,3.3-0.4,3.9,1.1 c0.6,1.6-1.2,5.3-5.8,11.3c-7.4,9.9-8.3,9-2.1,14.7c5.9,5.5,7.1,8.1,7.3,8.5C93.4,80.4,88.2,80.7,88.2,80.7z"></path></svg></span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="https://website397985.nicepage.io/pt/Page-9.html?version=cf10a508-18e4-4999-b5f7-60b9f96d0aaf#"><span class="u-icon u-icon-circle u-social-icon u-social-youtube u-icon-5"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112"><use href="#svg-547c"></use></svg><svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-547c" class="u-svg-content"><path d="M65.9,55.7l-17.6-8.9c-0.5-0.2-1,0.1-1,0.6v17.3c0,0.5,0.5,0.9,1,0.6L65.9,57C66.4,56.8,66.4,56,65.9,55.7z"></path><path d="M56.1,1.1c-30.4,0-55,24.6-55,55c0,30.4,24.6,55,55,55c30.4,0,55-24.6,55-55C111.1,25.7,86.5,1.1,56.1,1.1z M88.3,65.5 c0,7.4-6,13.4-13.4,13.4H37.3c-7.4,0-13.4-6-13.4-13.4V46.7c0-7.4,6-13.4,13.4-13.4h37.6c7.4,0,13.4,6,13.4,13.4V65.5z"></path></svg></span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+
+
+    <iframe width='853' height='480' src='https://my.matterport.com/show/?m=PiadgWdecdG' frameborder='0' allowfullscreen allow='xr-spatial-tracking'></iframe>
+    
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span> criado com </span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  
+</body></html>
